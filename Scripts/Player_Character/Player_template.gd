1a886008ea859a1f4c85c4ec2691bd1e82d6c881
peppy10003 <peppy10003@gmail.com>
2024-06-24 00:21:43 +0100

    fixed Movement issues, added menu, updated localization files

--- Scripts/Player_Character/Player_template.gd
+++ Scripts/Player_Character/Player_template.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
 const Base_Speed = 5
-const Focus_Power = 0.5
+const Focus_Speed = 2.5
 
 func _physics_process(delta):
 	var input_vector = Vector2(0,0)
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	if (Focus_Power * Input.get_action_strength("Focus")) == 0:
-		velocity = (input_vector * Base_Speed) * Focus_Power
-	velocity = (input_vector * Base_Speed)
+	if Input.get_action_strength("Focus") != 0:
+		velocity = (input_vector * Focus_Speed)
+	else:
+		velocity = (input_vector * Base_Speed)
 	move_and_collide(velocity)
